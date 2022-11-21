8b3ab9c4bea3c4a41255539dbd2b740f1346c964
Simon Wilhelm Schübel <siwischbl@gmail.com>
2022-11-21 18:58:28 +0100

    Did a lot of code cleanup. Created a Goal-Class, so both end-configurations can be accessed in the same file.

---
+++ 8puzzle.py
@@ -0,0 +1,127 @@
+import math
+import sys
+from queue import PriorityQueue
+import time
+from random import randrange 
+from utilities import PrioQueue, ClosedQueue, SearchTree, getRandomBoard, printBoard, getLegalMoves, getCopy, doXRandomMoves, doMove
+
+#board = [[], [], []]
+
+class Config:
+    def __init__(self, config):
+        if config == 1 or config == 2:
+            self.config = config
+        else:
+            raise ValueError
+    
+    def getGoalConfig(self):
+        if self.config == 1:
+            return [[0,1,2], [3,4,5], [6,7,8]]
+        if self.config == 2:
+            return [[1,2,3], [4,5,6], [7,8,0]]
+    
+    def getFinalPositions(self):
+        if self.config == 1:
+            return [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
+        if self.config == 2:
+            return [(2, 2), (0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1)]
+
+
+try:
+    GOAL_BOARD_CONFIGURATION = int(sys.argv[1])
+except:
+    print("Please enter a desired goal-configuration [1,2]; for example : python3 8puzzle.py 1")
+    exit()
+    
+CONFIG = Config(GOAL_BOARD_CONFIGURATION)
+FINAL_POSITIONS = CONFIG.getFinalPositions()
+GOAL_CONFIG = CONFIG.getGoalConfig()
+
+def accManhattenDistance(board):
+    amd = 0
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 0:
+                continue
+            amd += manhattenDistance(board[i][j], i, j)
+    return amd
+
+def manhattenDistance(n, currentI, currentJ):
+    return int(math.ceil(
+        math.sqrt(
+            math.pow(currentI - FINAL_POSITIONS[n][0], 2) + 
+            math.pow(currentJ - FINAL_POSITIONS[n][1], 2)
+        )
+    ))
+
+
+
+#-----------------------------------------------------------Implementing the algorithm---------------------------
+
+def isSolved(board):
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] != GOAL_CONFIG[i][j]:
+                return False 
+    return True
+
+searchTree = SearchTree()
+
+def findSolution(board):
+
+    
+    papaNode = searchTree.insert(board)
+
+    openQueue = PrioQueue()
+    closedQueue = ClosedQueue()
+    openQueue.insert(papaNode.getDepth() + accManhattenDistance(board), board)
+    counter = 0
+    while not openQueue.empty():
+        counter += 1
+        current_board = openQueue.getMin()[1] #get the board with min eval
+        current_tree_node = searchTree.getTreeNode(current_board)
+
+        if isSolved(current_board):
+            return current_tree_node 
+
+        #get all the possible boards, reachable from the current board and insert them into queue
+        for move in getLegalMoves(current_board):
+            bn = getCopy(current_board)
+            doMove(bn, move)
+            if not closedQueue.contains(bn):
+
+                node = searchTree.insert(bn)
+                node.setParent(current_tree_node)
+
+                openQueue.insert(node.getDepth() + accManhattenDistance(bn), bn)
+        closedQueue.insert(current_board)
+
+
+
+
+def main():
+    start_time = time.time()
+    
+    
+    solutionNode = findSolution(getRandomBoard())
+    #solutionNode = findSolution(doXRandomMoves(GOAL_CONFIG, 20))
+
+    listToParent = solutionNode.getListToParent()
+
+    print("Amout moves to solve : {}".format(len(listToParent) - 1))
+
+    print("Total Explored Nodes : {}".format(searchTree.getAmountNodes()))
+    
+
+
+    print("--- %s seconds ---" % (time.time() - start_time))
+    print("\n\n")
+
+    answer = input("Print moves to solution? [Y/N]")
+    if answer == "y" or answer == "Y":
+        for node in listToParent:
+            printBoard(node.getBoard())
+            print("\n")
+
+
+main()
--- utilities.py
+++ utilities.py
@@ -1,32 +1,23 @@
-import math
 from queue import PriorityQueue
-import time
 from random import randrange 
-
-#board = [[], [], []]
-
-
-FINAL_POSITIONS = [[2,2]]
-def getFinalPositions():
-    global FINAL_POSITIONS
-    for i in range(8):
-        FINAL_POSITIONS.append([int((i) / 3), (i) % 3])
-getFinalPositions()
 
 
 def getRandomBoard():
     return [[7, 2, 4], [5, 0, 6], [8, 3, 1]]
 
-def doXRandomMoves(x):
+def doXRandomMoves(startConfig, x):
     
-    b = [[1,2,3], [4,5,6], [7,8,0]]
+    b = getCopy(startConfig)
 
     for i in range(x):
         legalMoves = getLegalMoves(b)
         move = legalMoves[randrange(len(legalMoves))]
-        b[move[1][0]][move[1][1]] = b[move[0][0]][move[0][1]]
-        b[move[0][0]][move[0][1]] = 0
+        doMove(b, move)
     return b
+
+def doMove(board, move):
+    board[move[1][0]][move[1][1]] = board[move[0][0]][move[0][1]]
+    board[move[0][0]][move[0][1]] = 0
 
 def printBoard(board):
     boardString = ""
@@ -74,25 +65,7 @@ def getLegalMoves(board):
     return legalMoves
 
 
-def accManhattenDistance(board):
-    amd = 0
-    for i in range(len(board)):
-        for j in range(len(board)):
-            if board[i][j] == 0:
-                continue
-            amd += manhattenDistance(board[i][j], i, j)
-    return amd
 
-"""
-Cacculate the manhattenDistance of a number n on the board
-"""
-def manhattenDistance(n, currentI, currentJ):
-    return int(math.ceil(
-        math.sqrt(
-            math.pow(currentI - FINAL_POSITIONS[n][0], 2) + 
-            math.pow(currentJ - FINAL_POSITIONS[n][1], 2)
-        )
-    ))
 
 
 
@@ -108,17 +81,6 @@ class PrioQueue:
         self.q = PriorityQueue()
         self.inserted = ClosedQueue()
 
-    def insert(self, board):
-        key = ClosedQueue.getKey(board)
-
-        if self.inserted.contains(key):
-            return
-
-        md = accManhattenDistance(board)
-        self.q.put(
-            (md, board)
-        )
-        self.inserted.insert(key)
     
     def insert(self, prio, board):
         key = ClosedQueue.getKey(board)
@@ -140,6 +102,10 @@ class ClosedQueue:
     def __init__(self):
         self.closed = []
 
+    """
+    basically a hash-function for boards, by mapping each board onto a unique string
+    -> probably slow
+    """
     def getKey(board):
         k = ""
         for i in range(len(board)):
@@ -213,14 +179,11 @@ check if board equals the following array:
     [6,7,8]
 ]
 """
-solved = [[1,2,3],
-    [4,5,6],
-    [7,8,0]]
-
 def isSolved(board):
     for i in range(len(board)):
         for j in range(len(board)):
-            if board[i][j] != solved[i][j]:
+            
+            if board[i][j] != i*3 +j:
                 return False 
     return True
 
@@ -233,65 +196,3 @@ def getCopy(board):
             copy[i].append(board[i][j])
     return copy
 
-searchTree = SearchTree()
-
-def findSolution(board):
-
-    
-    papaNode = searchTree.insert(board)
-
-    openQueue = PrioQueue()
-    closedQueue = ClosedQueue()
-    openQueue.insert(papaNode.getDepth() + accManhattenDistance(board), board)
-    counter = 0
-    while not openQueue.empty():
-        counter += 1
-        current_board = openQueue.getMin()[1] #get the board with min eval
-        current_tree_node = searchTree.getTreeNode(current_board)
-
-        if isSolved(current_board):
-            return current_tree_node 
-
-        #get all the possible boards, reachable from the current board and insert them into queue
-        for move in getLegalMoves(current_board):
-            bn = getCopy(current_board)
-            bn[move[1][0]][move[1][1]] = bn[move[0][0]][move[0][1]]
-            bn[move[0][0]][move[0][1]] = 0
-            if not closedQueue.contains(bn):
-
-                node = searchTree.insert(bn)
-                node.setParent(current_tree_node)
-
-                openQueue.insert(node.getDepth() + accManhattenDistance(bn), bn)
-        closedQueue.insert(current_board)
-
-
-
-
-def main():
-    start_time = time.time()
-
-
-    #solutionNode = findSolution(boardToSolve)
-    #print(isSolved(doXRandomMoves(0)))
-    solutionNode = findSolution(getRandomBoard())
-    listToParent = solutionNode.getListToParent()
-
-    print("Amout moves to solve : {}".format(len(listToParent) - 1))
-
-    print("Total Explored Nodes : {}".format(searchTree.getAmountNodes()))
-    
-
-
-    print("--- %s seconds ---" % (time.time() - start_time))
-    print("\n\n\n")
-
-    answer = input("Print moves to solution?[Y/N]")
-    if answer == "y" or answer == "Y":
-        for node in listToParent:
-            printBoard(node.getBoard())
-            print("\n")
-
-
-main()
-#print(accManhattenDistance(getRandomBoard()))
